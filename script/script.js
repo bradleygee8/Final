33fec7c8773a3681f04fdc1e0a967263e68e2c87
bradleygee8 <71402232+bradleygee8@users.noreply.github.com>
2020-12-09 22:05:42 -0800

    scripting sheet

---
+++ script/script.js
@@ -0,0 +1,38 @@
+const hidden = document.querySelectorAll('.hidden')
+const displayed = document.querySelectorAll('.displayed-img')
+const button = document.querySelector('button')
+const textarea = document.getElementById('effect')
+const images = document.querySelector("section")
+const display = document.querySelector('.display')
+
+images.addEventListener('mouseover',display_image)
+button.addEventListener("click", effect);
+
+function display_image(e){
+    image = String(e.target.src).split("/").pop()
+    if (image != 'undefined')
+        display.src = 'images/' + image
+ 
+}
+
+function effect(){ 
+    if (textarea.value == 'blur'){
+        image = display.src;
+        image = image.split('/').pop()
+        index = image.lastIndexOf('.')
+        image = image.split('')
+        image [index] = 'B.'
+        temp =''
+        for(i = 0 ; i < image.length; i++){
+            temp += image[i]
+        }
+        image = temp
+        display.src = 'images/' +image
+        textarea.value = ''}
+    else {
+        alert("Invalid letter, please enter a different effect")
+        textarea.value =''
+
+    }
+    }
+
